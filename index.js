f1b459c26f2ce43270b0d26760183c6100a078a7
mostafahamed15 <mostafahamed73@yahoo.com>
2018-09-27 11:05:40 +0200

    make some comments to make code more readable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//acl Module
 function ACL() {
-
+    // empty object you can create roles and put on it
 	this.roles = {};
-
+    // variable a can create role.
 	this.a  = function(roleName) {
 		if (this.roles.hasOwnProperty(roleName) === false) {
 			throw "Role does not exist!";
@@ -12,7 +13,7 @@ function ACL() {
 	};
 
 	this.an = this.a;
-
+    // Function can return permission and http verps.
 	this.can = function(permission) {
 		if (['get', 'post', 'put', 'delete', 'patch'].indexOf(permission.toLowerCase()) === -1) {
 			throw "Invalid permission!";
@@ -21,7 +22,7 @@ function ACL() {
 		this.permission = permission;
 		return this;
 	}
-
+    // With function form you can return route or end point
 	this.from = function(route) {
 		if(this.roles[this.roleName][this.permission].indexOf(route) === -1) {
 			this.roles[this.roleName][this.permission].push(route)
@@ -50,7 +51,7 @@ function ACL() {
 			delete this.roles[roleName];
 		}
 	};
-
+    // function check can make test on roles
 	var Check = function(roles) {
 		this.roles = roles;
 		this.if = function(roleName) {
